f7991cbfb5f35651de04971ad6bf3ef2bd658fe6
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-12-02 11:05:51 +0000

    Day 2 | Proved part two example

--- implementation/src/main/kotlin/io/github/tomplum/aoc/report/ReportAnalyser.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/report/ReportAnalyser.kt
@@ -3,22 +3,38 @@ package io.github.tomplum.aoc.report
 import kotlin.math.abs
 
 class ReportAnalyser(private val data: List<String>) {
-    fun getSafeReports(): List<String> {
+    fun getSafeReports(): List<String> = data.filter { isReportSafe(it) }
+
+    fun getSafeReportsWithProblemDampener(): List<String> {
         return data.filter { line ->
             val values = line.split(" ").map { it.trim().toInt() }
-            val sorted = values.sorted()
-            val sortedDesc = values.sortedDescending()
 
-            var hasAdjacencySafety = true
+            val singleLevelRemoved = values.indices.map { i ->
+                val originalReportValues = values.toMutableList()
+                originalReportValues.removeAt(i)
+                originalReportValues
+            }.map { it.joinToString(" ") }
 
-            values.windowed(2) { (a, b) ->
-                val delta = abs(a - b)
-                if (delta < 1 || delta > 3) {
-                    hasAdjacencySafety = false
-                }
-            }
+            val reportPermutations = singleLevelRemoved + values.joinToString(" ")
 
-            (sorted == values || sortedDesc == values) && hasAdjacencySafety
+            reportPermutations.any { isReportSafe(it) }
         }
+    }
+
+    private fun isReportSafe(report: String): Boolean {
+        val values = report.split(" ").map { it.trim().toInt() }
+        val sorted = values.sorted()
+        val sortedDesc = values.sortedDescending()
+
+        var hasAdjacencySafety = true
+
+        values.windowed(2) { (a, b) ->
+            val delta = abs(a - b)
+            if (delta < 1 || delta > 3) {
+                hasAdjacencySafety = false
+            }
+        }
+
+        return (sorted == values || sortedDesc == values) && hasAdjacencySafety
     }
 }
--- implementation/src/test/kotlin/io/github/tomplum/aoc/report/ReportAnalyserTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/report/ReportAnalyserTest.kt
@@ -13,4 +13,12 @@ class ReportAnalyserTest {
         val safeReports = reportAnalyser.getSafeReports()
         assertThat(safeReports).hasSize(2)
     }
+
+    @Test
+    fun examplePartTwo() {
+        val reportData = TestInputReader.read<String>("/day2/example-1.txt").value
+        val reportAnalyser = ReportAnalyser(reportData)
+        val safeReports = reportAnalyser.getSafeReportsWithProblemDampener()
+        assertThat(safeReports).hasSize(4)
+    }
 }
