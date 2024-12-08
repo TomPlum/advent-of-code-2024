32d60242b30c547ba9456a689fae3188bd75eab3
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2024-12-08 14:20:22 +0000

    Day 8 | Added part one solution

---
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day8Test.kt
@@ -0,0 +1,12 @@
+package io.github.tomplum.aoc.solutions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class Day8Test {
+    @Test
+    fun part1() {
+        assertThat(Day8().part1()).isEqualTo(265)
+    }
+}
